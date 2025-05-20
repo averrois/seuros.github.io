3121e5ad1ce138bb4fede881eeeff2c7075ed5b7
Yassine Ait Rahou <70775489+averrois@users.noreply.github.com>
2025-05-20 11:17:04 +0100

    fix: remove rss from astro intergrations array

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import rss from "@astrojs/rss";
 
 const baseConfig = {
   site: "https://seuros.com",
-  integrations: [mdx(), sitemap(), tailwind(), rss()],
+  integrations: [mdx(), sitemap(), tailwind()],
   image: { service: passthroughImageService() },
 };
 
